3856eaed72a8611f174c9e154a0f856e4c2fd574
Isa <eisasparoo7@gmail.com>
2026-02-20 12:06:06 +0430

    Implement Telegram bot with start command
    Added a Telegram bot that responds to the 'start' command.

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import os
 from dotenv import load_dotenv
+import telebot
 
 load_dotenv()  # فایل .env رو می‌خونه
 TOKEN = os.getenv("TOKEN")
 
-print(TOKEN)  # فقط برای تست که درست خوند شده
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(msg):
+    bot.reply_to(msg, " سلام بچه ها به وانپیس رول خوش اومدید")
+
+bot.polling()
